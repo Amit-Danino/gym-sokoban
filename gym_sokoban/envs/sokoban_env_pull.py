b10c7808aa1c8c67a1e91ea6df5de92ce6da2472
Amit Danino <amit.danino@post.idc.ac.il>
2023-08-14 15:12:48 +0300

    no double resets

--- gym_sokoban/envs/sokoban_env_pull.py
+++ gym_sokoban/envs/sokoban_env_pull.py
@@ -17,7 +17,7 @@ class PushAndPullSokobanEnv(SokobanEnv):
              regen_room = False,
              observation = 'rgb_array'):
 
-        super(PushAndPullSokobanEnv, self).__init__(dim_room, max_steps, num_boxes, num_gen_steps, regen_room, observation)
+        super(PushAndPullSokobanEnv, self).__init__(dim_room, max_steps, num_boxes, num_gen_steps, regen_room, observation, reset=False)
         # screen_height, screen_width = (dim_room[0] * 16, dim_room[1] * 16)
         screen_height, screen_width = (dim_room[0], dim_room[1])
 
